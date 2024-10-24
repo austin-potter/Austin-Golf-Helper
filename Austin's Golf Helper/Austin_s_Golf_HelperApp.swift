b7ab373b09520f550133956106e920ce566ea041
Austin Potter <austinpotter33@gmail.com>
2024-10-23 20:36:26 -0500

    initial commit

---
+++ Austin's Golf Helper/Austin_s_Golf_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Austin_s_Golf_HelperApp.swift
+//  Austin's Golf Helper
+//
+//  Created by Austin Potter on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Austin_s_Golf_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Austin's Golf Helper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Austin's Golf Helper
+//
+//  Created by Austin Potter on 10/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
